a43f2e07e12346957dd2920fcb5f2cbaf8d1010c
Cologler <skyoflw@gmail.com>
2023-11-24 17:06:56 +0800

    refactor

--- src/index.ts
+++ src/index.ts
@@ -2,56 +2,51 @@ import "@logseq/libs";
 import { BlockEntity } from "@logseq/libs/dist/LSPlugin";
 
 async function main () {
-    type Comparer = (a: BlockEntity, b: BlockEntity) => number;
+    type Comparer = (a: string, b: string) => number;
 
     async function sortBlocks(source: BlockEntity[], comparer: Comparer) {
-        if (!source.length) {
+        if (source.length === 0 || source.some(x => typeof x.content !== 'string')) {
             return
         }
-
-        console.debug("sortBlocks", source, comparer);
 
         let fromArray = Array.from(source);
+        // console.debug() is async print, fromArray is empty when printing.
+        console.debug("Sort blocks", source);
 
-        const toArray = fromArray
-            .filter(x => x.content !== undefined)
-            .sort(comparer);
-
-        console.assert(fromArray.length === toArray.length);
-        if (fromArray.length !== toArray.length) {
-            // ?
-            return
-        }
-
-        console.log("sortedBlocks", toArray);
+        const toArray = Array.from(source).sort((a, b) => comparer(a.content, b.content));
+        console.log("Sorted blocks", toArray);
 
         for (let index = 0; index < toArray.length; index++) {
             const block = toArray[index];
             if (fromArray[0] !== block) {
                 if (index === 0) {
+                    // move before first one
                     await logseq.Editor.moveBlock(block.uuid, fromArray[0].uuid, { before: true })
                 }
                 else {
+                    // move after previous
                     await logseq.Editor.moveBlock(block.uuid, toArray[index-1].uuid);
                 }
                 fromArray = fromArray.filter(x => x !== block);
             }
             else {
+                // already in the right place
                 fromArray.shift()
             }
         }
     }
 
     const comparers = new Map<string, Comparer>([
-        ["A-Z", (a: BlockEntity, b: BlockEntity) => a.content.localeCompare(b.content, "en", { numeric: true })],
-        ["Z-A", (a: BlockEntity, b: BlockEntity) => -a.content.localeCompare(b.content, "en", { numeric: true })],
+        ["A-Z", (a: string, b: string) => a.localeCompare(b, "en", { numeric: true })],
+        ["Z-A", (a: string, b: string) => -a.localeCompare(b, "en", { numeric: true })],
     ]);
 
     for (const key of comparers.keys()) {
         const comparer = comparers.get(key)!;
+        const menuItemLabel = `Sort blocks: ${key}`;
 
         logseq.beforeunload(
-            logseq.Editor.registerBlockContextMenuItem(`Sort blocks: ${key}`, async (e) => {
+            logseq.Editor.registerBlockContextMenuItem(menuItemLabel, async (e) => {
                 const block = await logseq.Editor.getBlock(e.uuid, { includeChildren: true });
                 if (block) {
                     await sortBlocks(block.children as BlockEntity[], comparer)
@@ -60,7 +55,7 @@ async function main () {
         );
 
         logseq.beforeunload(
-            logseq.App.registerPageMenuItem(`Sort blocks: ${key}`, async ({ page }) => {
+            logseq.App.registerPageMenuItem(menuItemLabel, async ({ page }) => {
 
                 // `logseq.Editor.getPage(page, { includeChildren: true });`
                 // will return a null children
